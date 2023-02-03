55411d8ece9fe87736d87d4a9d4efce7bd6f2157
SNI69 <117408961+SNI69@users.noreply.github.com>
2023-02-03 12:50:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
   var urlObj = new window.URL(window.location.href);
-  var url = "https://ejvd3326248pklq0mtj313irgbc2vsrb-a-sites-opensocial.googleusercontent.com/gadgets/ifr?url=https://sites.google.com/site/s035r8h4/1v1.xml";
-  if (url) {
+  var url1 = "https://ejvd3326248pklq0mtj313irgbc2vsrb-a-sites-opensocial.googleusercontent.com/gadgets/ifr?url=https://sites.google.com/site/s035r8h4/1v1.xml";
+  if (url1) {
     var win;
-    document.querySelector('div.1v1').onclick = function() {
+    document.querySelector('div.card').onclick = function() {
       {
         win = window.open();
         win.document.body.style.margin = '0';
@@ -12,7 +12,28 @@
         iframe.style.width = '100%';
         iframe.style.height = '100%';
         iframe.style.margin = '0';
-        iframe.src = url;
+        iframe.src = url1;
+        win.document.body.appendChild(iframe);
+
+      }
+    }
+  }
+  
+  var urlObj = new window.URL(window.location.href);
+  var url2 = "https://sni69.github.io/slope";
+  if (url2) {
+    var win;
+    document.querySelector('div.card1').onclick = function() {
+      {
+        win = window.open();
+        win.document.body.style.margin = '0';
+        win.document.body.style.height = '100vh';
+        var iframe = win.document.createElement('iframe');
+        iframe.style.border = 'none';
+        iframe.style.width = '100%';
+        iframe.style.height = '100%';
+        iframe.style.margin = '0';
+        iframe.src = url2;
         win.document.body.appendChild(iframe);
 
       }
