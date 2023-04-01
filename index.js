3d76b239f34bacac3b082880c3becaacb9dd21c4
SNI69 <117408961+SNI69@users.noreply.github.com>
2023-04-01 11:08:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,83 @@
 var urlObj = new window.URL(window.location.href);
-  var url1 = "https://never-defeated-brothers.theballinballer.repl.co/service/hvtrs8%2F-1t1%2Clml-";
-  if (url1) {
-    var win;
-    document.querySelector('div.card1').onclick = function() {
-      {
-        win = window.open();
-        win.document.body.style.margin = '0';
-        win.document.body.style.height = '100vh';
-        var iframe = win.document.createElement('iframe');
-        iframe.style.border = 'none';
-        iframe.style.width = '100%';
-        iframe.style.height = '100%';
-        iframe.style.margin = '0';
-        iframe.src = url1;
-        win.document.body.appendChild(iframe);
+var url1 = "https://ejvd3326248pklq0mtj313irgbc2vsrb-a-sites-opensocial.googleusercontent.com/gadgets/ifr?url=https://sites.google.com/site/s035r8h4/1v1.xml";
+if (url1) {
+  var win;
+  document.querySelector('div.card1').onclick = function() {
+    {
+      win = window.open();
+      win.document.body.style.margin = '0';
+      win.document.body.style.height = '100vh';
+      var iframe = win.document.createElement('iframe');
+      iframe.style.border = 'none';
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.style.margin = '0';
+      iframe.src = url1;
+      win.document.body.appendChild(iframe);
 
-      }
     }
   }
-  
-  var urlObj = new window.URL(window.location.href);
-  var url2 = "https://sni69.github.io/slope";
-  if (url2) {
-    var win;
-    document.querySelector('div.card2').onclick = function() {
-      {
-        win = window.open();
-        win.document.body.style.margin = '0';
-        win.document.body.style.height = '100vh';
-        var iframe = win.document.createElement('iframe');
-        iframe.style.border = 'none';
-        iframe.style.width = '100%';
-        iframe.style.height = '100%';
-        iframe.style.margin = '0';
-        iframe.src = url2;
-        win.document.body.appendChild(iframe);
+}
 
-      }
+var urlObj = new window.URL(window.location.href);
+var url2 = "https://sni69.github.io/slope";
+if (url2) {
+  var win;
+  document.querySelector('div.card2').onclick = function() {
+    {
+      win = window.open();
+      win.document.body.style.margin = '0';
+      win.document.body.style.height = '100vh';
+      var iframe = win.document.createElement('iframe');
+      iframe.style.border = 'none';
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.style.margin = '0';
+      iframe.src = url2;
+      win.document.body.appendChild(iframe);
+
     }
   }
+}
+
+var urlObj = new window.URL(window.location.href);
+var url3 = "https://retro-bowl.masonsquadrito.repl.co/";
+if (url3) {
+  var win;
+  document.querySelector('div.card3').onclick = function() {
+    {
+      win = window.open();
+      win.document.body.style.margin = '0';
+      win.document.body.style.height = '100vh';
+      var iframe = win.document.createElement('iframe');
+      iframe.style.border = 'none';
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.style.margin = '0';
+      iframe.src = url3;
+      win.document.body.appendChild(iframe);
+
+    }
+  }
+}
+
+var urlObj = new window.URL(window.location.href);
+var url4 = "https://webglmath.github.io/burnin-rubber-5-xs/";
+if (url4) {
+  var win;
+  document.querySelector('div.card4').onclick = function() {
+    {
+      win = window.open();
+      win.document.body.style.margin = '0';
+      win.document.body.style.height = '100vh';
+      var iframe = win.document.createElement('iframe');
+      iframe.style.border = 'none';
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.style.margin = '0';
+      iframe.src = url4;
+      win.document.body.appendChild(iframe);
+
+    }
+  }
+}
